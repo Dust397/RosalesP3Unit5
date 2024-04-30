815b92cd009f558972f9a346c33205835f235130
Dust397 <752139@student.dusd.net>
2024-04-30 11:42:11 -0700

    Update score method

--- Unit5P3Rosales/Assets/Scripts/GameManager.cs
+++ Unit5P3Rosales/Assets/Scripts/GameManager.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     private float spwanRate = 1.0f;
+    public List<GameObject> targets;
+    private int score;
+    public TextMeshProUGUI ScoreText;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(0);
     }
 
     // Update is called once per frame
@@ -23,8 +29,19 @@ public class GameManager : MonoBehaviour
 
 
             yield return new WaitForSeconds(spwanRate);
-            int index = Random.Range(0, Target.Count);
+            int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
+            UpdateScore(5);
         }
     }
+    private void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        ScoreText.text = "Score:" + score;
+    }
+
+
+
+
+
 }
