63444a9051a96bdca01aa4590581b854a128295f
Dust397 <752139@student.dusd.net>
2024-05-08 11:37:46 -0700

    started whats the difficulty
    fin others steps

--- Unit5P3Rosales/Assets/Scripts/GameManager.cs
+++ Unit5P3Rosales/Assets/Scripts/GameManager.cs
@@ -2,9 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public Button restartButton;
+    public bool isGameActive;
+    public TextMeshProUGUI gameOverText;
     private float spwanRate = 1.0f;
     public List<GameObject> targets;
     private int score;
@@ -12,6 +17,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        isGameActive = true;
         StartCoroutine(SpawnTarget());
         score = 0;
         UpdateScore(0);
@@ -24,23 +30,31 @@ public class GameManager : MonoBehaviour
     }
     IEnumerator SpawnTarget()
     {
-        while (true)
+        while (isGameActive)
         {
-
+            
 
             yield return new WaitForSeconds(spwanRate);
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
-            UpdateScore(5);
+            
         }
     }
-    private void UpdateScore(int scoreToAdd)
+    public void UpdateScore(int scoreToAdd)
     {
         score += scoreToAdd;
         ScoreText.text = "Score:" + score;
     }
-
-
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
+        restartButton.gameObject.SetActive(true);
+    }
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
 
 
 
--- Unit5P3Rosales/Assets/Scripts/Target.cs
+++ Unit5P3Rosales/Assets/Scripts/Target.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
+    public int pointValue;
     private Rigidbody targetRb;
+    public ParticleSystem explosionParticle;
+    private GameManager gameManager;
     private float minSpeed = 12;
     private float maxSpeed = 16;
     private float maxTorque = 10;
@@ -13,6 +16,9 @@ public class Target : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+
+
         targetRb = GetComponent<Rigidbody>();
         targetRb.AddForce(Vector2.up * Random.Range(12, 16), ForceMode.Impulse);
         targetRb.AddTorque(Random.Range(-10, 10), Random.Range(-10, 10), Random.Range(-10, 10), ForceMode.Impulse);
@@ -27,11 +33,23 @@ public class Target : MonoBehaviour
     }
     private void OnMouseDown()
     {
-        Destroy(gameObject);
+        if (gameManager.isGameActive)
+        {
+
+
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+            Destroy(gameObject);
+            gameManager.UpdateScore(pointValue);
+        }
+
     }
     private void OnTriggerEnter(Collider other)
     {
         Destroy(gameObject);
+        if (!gameObject.CompareTag("Bad"))
+        {
+            gameManager.GameOver();
+        }
     }
     Vector3 RandomForce()
     {
